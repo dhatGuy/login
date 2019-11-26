adb05c906b0b31eb37d6159fe8efec63f5570f25
Odunsi Joseph <odunsiolakunbi@gmail.com>
2019-11-26 21:24:00 +0100

    add files

--- script.js
+++ script.js
@@ -12,14 +12,11 @@ password.addEventListener('blur', passwordVerify, true);
 
 // validation function
 function Validate() {
+  //regex conditions
   let usernameRegex = /^[a-zA-Z][0-9A-Za-z]+$/;
-  let firstChar = username.value.charAt(0);
+  let passwordRegex = /[A-Z].*\d|\d.*[A-Z]/;
 
-  // if(username.value.match(usernameRegex)){
-  //   alert('ok')
-  // }
-  
-  // validate username
+  // validate username if empty
   if (username.value == "") {
     username.style.border = "1px solid red";
     document.getElementById('username_div').style.color = "red";
@@ -27,7 +24,7 @@ function Validate() {
     username.focus();
     return false;
   }
-  // validate username
+  // validate username if less than 3 characters
   if (username.value.length < 3) {
     username.style.border = "1px solid red";
     document.getElementById('username_div').style.color = "red";
@@ -35,6 +32,17 @@ function Validate() {
     username.focus();
     return false;
   }
+
+  //validate username if character greater than 30
+  if (username.value.length > 30) {
+    username.style.border = "1px solid red";
+    document.getElementById('username_div').style.color = "red";
+    name_error.textContent = "Username is more than 30 characters";
+    username.focus();
+    return false;
+  }
+
+  //validate username if it starts with a letter
   if (username.value.match(usernameRegex) == null) {
       username.style.border = "1px solid red";
       document.getElementById('username_div').style.color = "red";
@@ -43,7 +51,7 @@ function Validate() {
       return false;
     }
     
-    // validate password
+    // validate password if it empty
     if (password.value == "") {
       password.style.border = "1px solid red";
       document.getElementById('password_div').style.color = "red";
@@ -51,8 +59,28 @@ function Validate() {
       password.focus();
       return false;
     }
+
+    // validate password if characters less than 8
+    if (password.value.length < 8) {
+      password.style.border = "1px solid red";
+      document.getElementById('password_div').style.color = "red";
+      password_error.textContent = "Password must be more than 8 characters";
+      password.focus();
+      return false;
+    }
+
+    // validate password if it contains an uppercase letter and a digit
+    if (password.value.match(passwordRegex) == null) {
+      password.style.border = "1px solid red";
+      document.getElementById('password_div').style.color = "red";
+      password_error.textContent = "Password must contain atleast an uppercase and a digit";
+      password.focus();
+      return false;
+    }
     
   }
+
+
   // event handler functions
   function nameVerify() {
     if (username.value != "") {
